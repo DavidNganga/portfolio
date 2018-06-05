74ff7eb9d458f6eb6f01632ac0d86d71715c50ad
DavidNganga <ngashiedavid@gmail.com>
2018-06-05 09:23:41 +0300

    modified models.py

--- folio/models.py
+++ folio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+     name = models.CharField(max_length=20)
+     link_url = models.TextField(blank = True)
+     description = models.CharField(max_length=200)
+     screenshot = models.ImageField(upload_to ='images/',null = True )
