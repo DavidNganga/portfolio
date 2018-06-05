eda42424d76782cd0ad49825fcb128ed2d8193fa
DavidNganga <ngashiedavid@gmail.com>
2018-06-05 12:38:10 +0300

    added bootstrap

---
+++ folio/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url,include
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns=[
+    url(r'^$',views.index,name='index'),
+    url(r'^viewproject/', views.viewproject, name='viewproject'),
+
+
+]
--- folio/views.py
+++ folio/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
+def index(request):
+
+    return render (request, 'index.html')
+
+def project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+            return redirect('index')
+    else:
+        form = ProfileForm()
+    return render(request, 'project.html', {"form": form})
+
+def viewproject(request):
+    projects = Project.get_all()
+    return render (request, 'viewproject.html', {"projects":projects})
